c7e9843e00830b83b8ff2e09d0653af411909563
Asianna Kennedy <kennedot@bc.edu>
2026-07-07 09:27:32 -0400

    Update app.py

--- app.py
+++ app.py
@@ -156,18 +156,22 @@ with tab2:
 
     # Data preparation for geochart
     state_fips = {
-        "Alabama": 1, "Alaska": 2, "Arizona": 3, "Arkansas": 4, "California": 5,
-        "Colorado": 6, "Connecticut": 7, "Delaware": 8, "District of Columbia": 9,
-        "Florida": 10, "Georgia": 11, "Hawaii": 12, "Idaho": 13, "Illinois": 14,
-        "Indiana": 15, "Iowa": 16, "Kansas": 17, "Kentucky": 18, "Louisiana": 19,
-        "Maine": 20, "Maryland": 21, "Massachusetts": 22, "Michigan": 23,
-        "Minnesota": 24, "Mississippi": 25, "Missouri": 26, "Montana": 27,
-        "Nebraska": 28, "Nevada": 29, "New Hampshire": 30, "New Jersey": 31,
-        "New Mexico": 32, "New York": 33, "North Carolina": 34, "North Dakota": 35,
-        "Ohio": 36, "Oklahoma": 37, "Oregon": 38, "Pennsylvania": 39,
-        "Rhode Island": 40, "South Carolina": 41, "South Dakota": 42,
-        "Tennessee": 43, "Texas": 44, "Utah": 45, "Vermont": 46, "Virginia": 47,
-        "Washington": 48, "West Virginia": 49, "Wisconsin": 50, "Wyoming": 51
+        "Alabama": "01", "Alaska": "02", "Arizona": "04", "Arkansas": "05",
+        "California": "06", "Colorado": "08", "Connecticut": "09",
+        "Delaware": "10", "District of Columbia": "11", "Florida": "12",
+        "Georgia": "13", "Hawaii": "15", "Idaho": "16", "Illinois": "17",
+        "Indiana": "18", "Iowa": "19", "Kansas": "20", "Kentucky": "21",
+        "Louisiana": "22", "Maine": "23", "Maryland": "24",
+        "Massachusetts": "25", "Michigan": "26", "Minnesota": "27",
+        "Mississippi": "28", "Missouri": "29", "Montana": "30",
+        "Nebraska": "31", "Nevada": "32", "New Hampshire": "33",
+        "New Jersey": "34", "New Mexico": "35", "New York": "36",
+        "North Carolina": "37", "North Dakota": "38", "Ohio": "39",
+        "Oklahoma": "40", "Oregon": "41", "Pennsylvania": "42",
+        "Rhode Island": "44", "South Carolina": "45", "South Dakota": "46",
+        "Tennessee": "47", "Texas": "48", "Utah": "49", "Vermont": "50",
+        "Virginia": "51", "Washington": "53", "West Virginia": "54",
+        "Wisconsin": "55", "Wyoming": "56"
     }
 
     geochart_data = filtered_race.dropna(subset=["Life Expectancy"]).copy()
